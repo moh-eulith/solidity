2a1706d802bb187c60efa0aaf432d9256c46ffaf
Matheus Aguiar <matheus.pit@gmail.com>
2025-02-13 15:05:30 -0300

    fixup! AST_accept.h

--- libsolidity/ast/AST_accept.h
+++ libsolidity/ast/AST_accept.h
@@ -1167,7 +1167,8 @@ void ForAllQuantifier::accept(ASTConstVisitor& _visitor) const
 
 void StorageLayoutSpecifier::accept(ASTVisitor& _visitor)
 {
-	if (_visitor.visit(*this) && m_baseSlotExpression)
+	if (_visitor.visit(*this))
+		// m_baseSlotExpression is asserted to be non-null during construction
 		m_baseSlotExpression->accept(_visitor);
 
 	_visitor.endVisit(*this);
@@ -1175,7 +1176,8 @@ void StorageLayoutSpecifier::accept(ASTVisitor& _visitor)
 
 void StorageLayoutSpecifier::accept(ASTConstVisitor& _visitor) const
 {
-	if (_visitor.visit(*this) && m_baseSlotExpression)
+	if (_visitor.visit(*this))
+		// m_baseSlotExpression is asserted to be non-null during construction
 		m_baseSlotExpression->accept(_visitor);
 
 	_visitor.endVisit(*this);
