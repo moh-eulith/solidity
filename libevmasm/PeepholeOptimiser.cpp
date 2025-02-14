62fe73e064c4bbfa8ab143d85fa75cca614a5a00
Mohammad Rezaei <moh@compositelabs.io>
2025-02-13 20:15:09 -0500

    Triple swap optimization

--- libevmasm/PeepholeOptimiser.cpp
+++ libevmasm/PeepholeOptimiser.cpp
@@ -461,6 +461,49 @@ struct DoubleRJump: SimplePeepholeOptimizerMethod<DoubleRJump>
 	}
 };
 
+// push swap2 swap1 swap2: -> swap1 push:
+// stack starts with [a, b, ...]
+// push x: [x, a, b, ...]
+// swap2: [b, a, x, ...]
+// swap1: [a, b, x, ...]
+// swap2: [x, b, a, ...]
+// instead:
+// swap1: [b, a, ...]
+// push x: [x, b, a, ...]
+struct TripleSwap: SimplePeepholeOptimizerMethod<TripleSwap>
+{
+	static size_t applySimple(
+		AssemblyItem const& _push,
+		AssemblyItem const& _swapa,
+		AssemblyItem const& _swapb,
+		AssemblyItem const& _swapc,
+		std::back_insert_iterator<AssemblyItems> _out
+	)
+	{
+		static std::map<Instruction, Instruction> const swapMinusOne{
+			{ Instruction::SWAP2, Instruction::SWAP1 },
+			{ Instruction::SWAP3, Instruction::SWAP2 },
+			{ Instruction::SWAP4, Instruction::SWAP3 },
+			{ Instruction::SWAP5, Instruction::SWAP4 }
+		};
+	
+		if (
+			_swapa == _swapc &&
+			_swapb == Instruction::SWAP1 &&
+			_swapa.type() == Operation &&
+			swapMinusOne.count(_swapa.instruction()) &&
+			(_push.type() == Push || _push.type() == PushTag)
+		)
+		{
+			*_out = AssemblyItem(swapMinusOne.at(_swapa.instruction()), _swapa.debugData());
+			*_out = _push;
+			return true;
+		}
+		else
+			return false;
+	}
+};
+
 struct JumpToNext: SimplePeepholeOptimizerMethod<JumpToNext>
 {
 	static size_t applySimple(
@@ -724,6 +767,7 @@ bool PeepholeOptimiser::optimise()
 		applyMethods(
 			state,
 			PushPop(),
+			TripleSwap(),
 			OpPop(),
 			OpStop(),
 			OpReturnRevert(),
